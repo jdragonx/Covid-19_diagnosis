0e03169de419c73bf8e0e4f0ee376bbf1f0b32a2
jdragonxepn <jonathan.herrera01@epn.edu.ec>
2020-09-12 18:46:47 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"jonherr21@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
